4c4cdfe075e9ce8e22e0567f268134591c98841a
Matheus Becari <matheus.becari.dias@escola.pr.gov.br>
2026-03-07 00:47:13 +0000

    app ŕpketp

---
+++ AppReact/assets/ClassComponents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ClassComponents extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Componentes de classe</h3>
+            </div>
+        )
+    }
+} 
+
+export default ClassComponents;
